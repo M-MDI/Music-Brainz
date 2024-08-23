d4986bd82fadfd037e9fd743587bdccbcc78acd8
mmoulabbi <Mehdi.nada909@gmail.com>
2024-08-23 15:11:47 +0100

    Beta Groupie: need more css touch

--- main.go
+++ main.go
@@ -10,6 +10,6 @@ func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.HandleFunc("/", Gr.HomeHandler)
 	http.HandleFunc("/artist/{id}", Gr.ArtistDetails)
-	log.Println("Server starting on : http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Println("Server starting on : http://localhost:8040")
+	log.Fatal(http.ListenAndServe(":8040", nil))
 }
